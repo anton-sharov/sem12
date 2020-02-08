989b782c1ce5c2b0b0f27f15badd444907f147fe
антон шаров <antoshka_sharov@mail.ru>
2020-02-08 14:28:07 +0500

    1+2

--- sem12/sem12.cpp
+++ sem12/sem12.cpp
@@ -4,7 +4,10 @@
 
 int main()
 {
-    std::cout << "Hello World" << std::endl; 
+    std::cout << "Hello World" << std::endl;
+	std::cout << "1+2" << std::endl;
+	std::cout << "Hello World" << std::endl;
+
     return 0;
 }
 
